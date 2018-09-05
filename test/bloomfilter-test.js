493e7c9c35981bb7439c580bf9c210ad8bee2a03
d-v <dimvasilk@gmail.com>
2018-09-04 20:47:11 -0500

    implemented majestic million bloom filter

--- test/bloomfilter-test.js
+++ test/bloomfilter-test.js
@@ -1,61 +1,50 @@
 var bf = require("../bloomfilter"),
-    BloomFilter = bf.BloomFilter,
-    fnv_1a = bf.fnv_1a;
+$ = require('jquery'),
+BloomFilter = bf.BloomFilter,
+fnv_1a = bf.fnv_1a,
+csv = require('csv'),
+fs = require('fs'),
+parse = require('csv-parse');
 
 var vows = require("vows"),
-    assert = require("assert");
+assert = require("assert");
+
+var inputFile='majestic_million.csv';
 
 var suite = vows.describe("bloomfilter");
 
-var jabberwocky = "`Twas brillig, and the slithy toves\n  Did gyre and gimble in the wabe:\nAll mimsy were the borogoves,\n  And the mome raths outgrabe.\n\n\"Beware the Jabberwock, my son!\n  The jaws that bite, the claws that catch!\nBeware the Jubjub bird, and shun\n  The frumious Bandersnatch!\"\n\nHe took his vorpal sword in hand:\n  Long time the manxome foe he sought --\nSo rested he by the Tumtum tree,\n  And stood awhile in thought.\n\nAnd, as in uffish thought he stood,\n  The Jabberwock, with eyes of flame,\nCame whiffling through the tulgey wood,\n  And burbled as it came!\n\nOne, two! One, two! And through and through\n  The vorpal blade went snicker-snack!\nHe left it dead, and with its head\n  He went galumphing back.\n\n\"And, has thou slain the Jabberwock?\n  Come to my arms, my beamish boy!\nO frabjous day! Callooh! Callay!'\n  He chortled in his joy.\n\n`Twas brillig, and the slithy toves\n  Did gyre and gimble in the wabe;\nAll mimsy were the borogoves,\n  And the mome raths outgrabe.";
+var domains = new Array();
 
-suite.addBatch({
-  "bloom filter": {
-    "basic": function() {
-      var f = new BloomFilter(1000, 4),
-          n1 = "Bess",
-          n2 = "Jane";
-      f.add(n1);
-      assert.equal(f.test(n1), true);
-      assert.equal(f.test(n2), false);
-    },
-    "jabberwocky": function() {
-      var f = new BloomFilter(1000, 4),
-          n1 = jabberwocky,
-          n2 = jabberwocky + "\n";
-      f.add(n1);
-      assert.equal(f.test(n1), true);
-      assert.equal(f.test(n2), false);
-    },
-    "basic uint32": function() {
-      var f = new BloomFilter(1000, 4),
-          n1 = "\u0100",
-          n2 = "\u0101",
-          n3 = "\u0103";
-      f.add(n1);
-      assert.equal(f.test(n1), true);
-      assert.equal(f.test(n2), false);
-      assert.equal(f.test(n3), false);
-    },
-    "wtf": function() {
-      var f = new BloomFilter(20, 10);
-      f.add("abc");
-      assert.equal(f.test("wtf"), false);
-    },
-    "works with integer types": function() {
-      var f = new BloomFilter(1000, 4);
-      f.add(1);
-      assert.equal(f.test(1), true);
-      assert.equal(f.test(2), false);
-    },
-    "size": function() {
-      var f = new BloomFilter(1000, 4), i = -1;
-      for (var i = 0; i < 100; ++i) f.add(i);
-      assert.inDelta(f.size(), 100, 6);
-      for (var i = 0; i < 1000; ++i) f.add(i);
-      assert.inDelta(f.size(), 1000, 100);
+var parser = parse({delimiter: ','}, function (err, data) {
+  console.log("adding domains");
+  data.forEach(function(line) {
+    domains.push(line[2]);
+  });
+  console.log("finished adding domains");
+  suite.addBatch({
+    "bloom filter": {
+      "basic": function() {
+        var f = new BloomFilter(1000, 4),
+        n1 = "Bess",
+        n2 = "Jane";
+        f.add(n1);
+        assert.equal(f.test(n1), true);
+        assert.equal(f.test(n2), false);
+      },
+      "majestic": function() {
+        var m = new BloomFilter(700000000, 100);
+        for (var i = 0; i < domains.length; i++) {
+          m.add(domains[i]);
+        }
+        assert.equal(m.test("google.com"), true);
+        assert.equal(m.test("thisisafakedomain"), false);
+      }
     }
-  }
+  });
+
+  suite.export(module);  
 });
 
-suite.export(module);
+fs.createReadStream(inputFile).pipe(parser);
+
+
